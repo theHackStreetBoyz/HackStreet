25b9bd376b977a64e350078917358370adc2caea
Max Brodheim <max.brodheim@gmail.com>
2017-07-24 17:22:48 -0400

    actually fixed semi-colons

--- app/main.jsx
+++ app/main.jsx
@@ -1,22 +1,22 @@
-'use strict';
+'use strict'
 
 /**
  * `babel-preset-env` converts this general import into a selection of specific
  * imports needed to polyfill the currently-supported environment (as specified
  * in `.babelrc`). As of 2017-06-04, this is primarily to support async/await.
  */
-import 'babel-polyfill';
+import 'babel-polyfill'
 
-import React from 'react';
-import {Router, Route, IndexRedirect, browserHistory} from 'react-router';
-import {render} from 'react-dom';
-import {connect, Provider} from 'react-redux';
+import React from 'react'
+import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
+import {render} from 'react-dom'
+import {connect, Provider} from 'react-redux'
 
-import store from './store';
-import Jokes from './components/Jokes';
-import Login from './components/Login';
-import WhoAmI from './components/WhoAmI';
-import NotFound from './components/NotFound';
+import store from './store'
+import Jokes from './components/Jokes'
+import Login from './components/Login'
+import WhoAmI from './components/WhoAmI'
+import NotFound from './components/NotFound'
 
 const ExampleApp = connect(
   ({ auth }) => ({ user: auth })
@@ -28,7 +28,7 @@ const ExampleApp = connect(
       </nav>
       {children}
     </div>
-);
+)
 
 render(
   <Provider store={store}>
@@ -41,4 +41,4 @@ render(
     </Router>
   </Provider>,
   document.getElementById('main')
-);
+)
