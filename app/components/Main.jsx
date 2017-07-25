ae32c4c2a6b3fc72f814febe211487ca409c1655
Alvin Wen <alvinwen424@gmail.com>
2017-07-25 17:09:35 -0400

    initial navbar

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -8,31 +8,35 @@
 import 'babel-polyfill'
 
 import React, { Component } from 'react'
-import {render} from 'react-dom'
-import {Provider, connect} from 'react-redux'
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+import { render } from 'react-dom'
+import { Provider, connect } from 'react-redux'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 import Home from './Home'
 import Artists from './Artists'
 import Albums from './Albums'
 import Songs from './Songs'
+import Navbar from './Navbar'
 
 
 export default class Main extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
-    render (){
-       return (
-           <div>
-               <Switch>
-                   <Route path='/artists' component={Artists} />
-                   <Route path='/albums' component={Albums} />
-                   <Route path='/songs' component={Songs} />
-                   <Route path='/' component={Home} />
-               </Switch>
-           </div>
-       )
-   }
+    render() {
+        return (
+            <div>
+                <div>
+                    <Navbar className="navbar-nav"/>
+                </div>
+                <Switch>
+                    <Route path='/artists' component={Artists} />
+                    <Route path='/albums' component={Albums} />
+                    <Route path='/songs' component={Songs} />
+                    <Route path='/' component={Home} />
+                </Switch>
+            </div>
+        )
+    }
 }
