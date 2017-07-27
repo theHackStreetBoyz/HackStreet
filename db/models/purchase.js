5ec4c531604536d0f67fece0affe47e3820dc58f
Max Brodheim <max.brodheim@gmail.com>
2017-07-27 15:29:43 -0400

    fix: integrates all of omri's edits from code review #1

--- db/models/purchase.js
+++ db/models/purchase.js
@@ -1,30 +1,8 @@
 const DataTypes = require('sequelize')
 
-module.exports = db => db.define('purchase', {
-  date: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW
-  },
-  songs: {
-    type: DataTypes.ARRAY(DataTypes.INTEGER),
-    defaultValue: []
-  }
-}, {
-  classMethods: {
-    newPurchaseFromCart(cart, userId) {
-      return db.model('purchase').create(cart)
-      .then(newCart => {
-        Promise.all([
-          newCart.setUser(userId),
-          newCart.getSongs().then(songs => newCart.setSongs(songs))
-        ])
-        return newCart
-      })
-    }
-  }
-})
+module.exports = db => db.define('purchase')
 
 module.exports.associations = (Purchase, {Song, User}) => {
   Purchase.belongsTo(User)
+  Purchase.belongsToMany(Song, {through: 'purchaseSong'})
 }
