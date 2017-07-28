de74ef9c96153c950e98e629e07523b5341193c7
Max Brodheim <max.brodheim@gmail.com>
2017-07-28 17:36:26 -0400

    starts work on cart

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -1,24 +1,38 @@
 import React, { Component } from 'react'
+import {BrowserRouter as Router, Switch, Route, Redirect, Link} from 'react-router-dom'
+import { connect } from 'react-redux'
 
-export default class Cart extends Component {
+export class Cart extends Component {
   render() {
     let totalPrice = 0
     return (
       <div>
         <h2>Cart:</h2>
-          {this.store.props.cart.songs.map(song => {
+        <div>
+          {this.props.auth.songs.map(song => {
             totalPrice += song.price
             return (
               <div>
-              <p>song.name</p>
-              <p>song.artist</p>
-              <p>song.album</p>
-              <p>song.price</p>
+                <p>song.name</p>
+                <p>song.artist</p>
+                <p>song.album</p>
+                <p>song.price</p>
               </div>
             )
           })}
           <p>{`Total Price: ${totalPrice}`}</p>
+        </div>
       </div>
     )
   }
 }
+
+function mapStateToProps(state, ownProps) {
+  console.log('STATE: ', state)
+  return {
+    auth: state.auth
+  }
+}
+const mapDispatch = {}
+const Container = connect(mapStateToProps, mapDispatch)(Cart)
+export default Container
