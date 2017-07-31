8763040f3fe87ed8cd4521dd1b388946a3015847
Alvin Wen <alvinwen424@gmail.com>
2017-07-31 11:30:18 -0400

    initial cartPage

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class Cart extends Component {
+import { fetchCart } from '../reducers/auth'
+
+
+
+class Cart extends Component {
+
+  componentDidMount(){
+  this.props.loadCart(this.props.auth.id)
+  }
   render() {
+    console.log(this.props.cart)
     let totalPrice = 0
     return (
       <div>
         <h2>Cart:</h2>
-          {/*this.store.props.cart.songs.map(song => {
+          this.props.cart.songs.map(song => {
             totalPrice += song.price
             return (
               <div>
@@ -16,9 +26,32 @@ export default class Cart extends Component {
               <p>song.price</p>
               </div>
             )
-          })*/}
+          })
           <p>{`Total Price: ${totalPrice}`}</p>
       </div>
     )
   }
 }
+
+
+const mapStateToProps = function (state) {
+  console.log(state)
+  return {
+    auth: state.auth,
+    cart: state.auth.cart
+  };
+};
+
+const mapDispatchToProps = function (dispatch) {
+  return {
+    loadCart: (id) => {
+      dispatch(fetchCart(id))
+    }
+  }
+}
+
+const cartContainer = connect(mapStateToProps, mapDispatchToProps)(Cart);
+
+export default cartContainer;
+
+
