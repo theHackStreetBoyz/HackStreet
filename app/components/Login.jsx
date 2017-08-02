3b1bf804f53ec6a1cdbbef93c350c798fd324215
Max Brodheim <max.brodheim@gmail.com>
2017-08-01 21:25:22 -0400

    adds facebook oauth login

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 
 export const Login = ({ login }) => (
+  <div>
   <form onSubmit={evt => {
     evt.preventDefault()
     login(evt.target.username.value, evt.target.password.value)
-  } }>
+  }}>
     <input name="username" />
     <input name="password" type="password" />
     <input type="submit" value="Login" />
   </form>
+    <a href="/api/auth/login/facebook"><button>facebook</button></a>
+    </div>
 )
 
-import {login} from 'APP/app/reducers/auth'
-import {connect} from 'react-redux'
+import { login } from 'APP/app/reducers/auth'
+import { connect } from 'react-redux'
 
 export default connect(
   state => ({}),
-  {login},
+  { login },
 )(Login)
